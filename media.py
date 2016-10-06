f807c46e859e01725fc1fac676993038736f2940
Harshit Sharma <geniusharshit@gmail.com>
2016-10-07 02:30:49 +0530

    first commit

---
+++ media.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+
+class Movie():
+	def __init__(self, m_title, m_storyline, m_poster, m_trailer):
+		self.title = m_title
+		self.storyline = m_storyline
+		self.poster_image_url = m_poster
+		self.trailer_youtube_url = m_trailer
+		
+	def show_trailer(self):
+		webbrowser.open(self.trailer_url)
---
+++ movie_website.py
@@ -0,0 +1,7 @@
+import entertainment
+import fresh_tomato
+import media
+
+movie_query = [entertainment.shivay, entertainment.avatar, entertainment.ae_dil, entertainment.ms_dhoni, entertainment.hobbit, entertainment.birdman]
+
+fresh_tomato.open_movies_page(movie_query)
