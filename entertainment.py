462e928422c598c43023e37de8e9548dd3f0a604
Harshit Sharma <geniusharshit@gmail.com>
2016-10-22 04:08:15 +0530

    sdfsf

--- entertainment.py
+++ entertainment.py
@@ -1,14 +1,26 @@
 import media
 
-shivay = media.Movie("Shivay","A story of a man and his life","https://upload.wikimedia.org/wikipedia/en/a/a1/Ajay_Devgan%27s_Shivaay_poster.jpg", "https://www.youtube.com/watch?v=poLjq0u4_5A")
+shivay = media.Movie("Shivay", "A story of a man and his life", \
+	"https://upload.wikimedia.org/wikipedia/en/a/a1/Ajay_Devgan%27s_Shivaay_poster.jpg", \
+	"https://www.youtube.com/watch?v=poLjq0u4_5A")
 
-avatar = media.Movie("Avatar","A marine on an alien planet","http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+avatar = media.Movie("Avatar", "A marine on an alien planet",\
+ "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp",\
+  "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-ae_dil = media.Movie("Ae dil hai mushkil","A love triangle", "https://upload.wikimedia.org/wikipedia/en/2/2a/Ae_Dil_Hai_Mushkil2.jpg","https://www.youtube.com/watch?v=Z_PODraXg4E")
+ae_dil = media.Movie("Ae dil hai mushkil", "A love triangle",\
+ "https://upload.wikimedia.org/wikipedia/en/2/2a/Ae_Dil_Hai_Mushkil2.jpg",\
+  "https://www.youtube.com/watch?v=Z_PODraXg4E")
 
-ms_dhoni = media.Movie("MS DHONI", "Untold Story of indian cricket team captain", "https://shyfyy.files.wordpress.com/2014/09/msdhoni1.jpg","https://www.youtube.com/watch?v=6L6XqWoS8tw")
+ms_dhoni = media.Movie("MS DHONI", "Untold Story of indian cricket team captain", \
+	"https://shyfyy.files.wordpress.com/2014/09/msdhoni1.jpg", \
+	"https://www.youtube.com/watch?v=6L6XqWoS8tw")
 
-hobbit = media.Movie("The Hobbit: An Unexpected Journey", "Bilbo Baggins (Martin Freeman) lives a simple life with his fellow hobbits in the shire, until the wizard Gandalf (Ian McKellen) arrives and convinces him to join a group of dwarves on a quest to reclaim the kingdom of Erebor.", "http://downloads.chrisullyott.com/courses/udacity/intro-to-programming/movie-posters/the-hobbit.jpg", "https://www.youtube.com/watch?v=SDnYMbYB-nU")
+hobbit = media.Movie("The Hobbit: An Unexpected Journey",\
+ "Bilbo Baggins (Martin Freeman) lives a simple life with his fellow hobbits in the shire, until the wizard Gandalf (Ian McKellen) arrives and convinces him to join a group of dwarves on a quest to reclaim the kingdom of Erebor.",\
+  "http://downloads.chrisullyott.com/courses/udacity/intro-to-programming/movie-posters/the-hobbit.jpg", \
+  "https://www.youtube.com/watch?v=SDnYMbYB-nU")
 
-birdman = media.Movie("Birdman", "Former cinema superhero Riggan Thomson (Michael Keaton) is mounting an ambitious Broadway production that he hopes will breathe new life into his stagnant career.", "http://downloads.chrisullyott.com/courses/udacity/intro-to-programming/movie-posters/birdman.jpg", "https://www.youtube.com/watch?v=uJfLoE6hanc")
-#avatar.show_trailer()
+birdman = media.Movie("Birdman", "Former cinema superhero Riggan Thomson (Michael Keaton) is mounting an ambitious Broadway production that he hopes will breathe new life into his stagnant career.",\
+ "http://downloads.chrisullyott.com/courses/udacity/intro-to-programming/movie-posters/birdman.jpg", \
+ "https://www.youtube.com/watch?v=uJfLoE6hanc")
